5abe74339f5f355cf34f337b931dd8ac7f604119
androemeda <kartikdeshpande2810@gmail.com>
2024-05-29 13:16:28 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
 
 function App() {
   return (
-    <>
-      Portfolio
-    </>
+    <div className="App">
+      <header className='App-header'>
+        <img src={logo} className="App-logo" alt="logo" />
+        <NavBar></NavBar>
+      </header>
+    </div>
   );
 }
 
