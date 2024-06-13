6e1580829928009be84635edf34345169d0458c8
androemeda <kartikdeshpande2810@gmail.com>
2024-06-13 18:33:07 +0530

    projects section completed

--- src/components/project-card/ProjectCard.jsx
+++ src/components/project-card/ProjectCard.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import './ProjectCard.css'
+import { FaGithub } from "react-icons/fa";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
-function ProjectCard({image , name}) {
+function ProjectCard({image , name , githubLink , deployedLink}) {
   return (
     <div className='project-card'>
+
       <div className='project-name'>{name}</div>
+
+
       <div className='project-image'>
+
         <img src={image} alt="" />
+
+        <div className='cover'>
+          <a href={githubLink}><FaGithub className='icon'/></a>
+          {deployedLink && <a href={deployedLink}><FaExternalLinkAlt className='icon'/></a>}
+        </div>
+
       </div>
+
     </div>
   )
 }
 
-export default ProjectCard
+export default ProjectCard;
--- src/pages/projects/Project.jsx
+++ src/pages/projects/Project.jsx
@@ -3,33 +3,36 @@ import ProjectCard from '../../components/project-card/ProjectCard';
 import './Project.css'
 import portfolioProjectImage from '../../assets/project-portfolio.png'
 import blankImage from '../../assets/blank.png'
+import imageEditor from '../../assets/image-editor.jpeg'
+import webScrapper from '../../assets/web-scraper.jpeg'
+import quizApp from '../../assets/quiz-app.png'
 
 function Project() {
 
   let allProjects = [
     {
       name : "PORTFOLIO-WEBSITE",
-      image : portfolioProjectImage
+      image : portfolioProjectImage,
+      githubLink : "https://github.com/androemeda/Portfolio",
+      deployedLink : "https://portfolio-ten-phi-45.vercel.app/"
     },
     {
-      name : "WILL BE UPDATED",
-      image : blankImage
+      name : "IMAGE-EDITOR",
+      image : imageEditor,
+      githubLink : "https://github.com/androemeda/image-editor",
+      deployedLink : null
     },
     {
-      name : "WILL BE UPDATED",
-      image : blankImage
+      name : "WEB-SCRAPER",
+      image : webScrapper,
+      githubLink : "https://github.com/androemeda/web_scraper_price-comparison",
+      deployedLink : null
     },
     {
-      name : "WILL BE UPDATED",
-      image : blankImage
-    },
-    {
-      name : "WILL BE UPDATED",
-      image : blankImage
-    },
-    {
-      name : "WILL BE UPDATED",
-      image : blankImage
+      name : "QUIZ-APP",
+      image : quizApp,
+      githubLink : "https://github.com/androemeda/Quiz-App?tab=readme-ov-file",
+      deployedLink : "https://androemeda.github.io/Quiz-App/"
     }
   ];
 
@@ -42,7 +45,7 @@ function Project() {
         {
           allProjects.map((project , index) => {
             return (
-              <ProjectCard name={project.name} image={project.image}></ProjectCard>
+              <ProjectCard name={project.name} image={project.image} githubLink={project.githubLink} deployedLink={project.deployedLink}></ProjectCard>
             )
           })
         }
